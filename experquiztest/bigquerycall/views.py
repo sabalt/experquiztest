655f73cfeda162a05fbdf3f81bb5a7dda4916820
Tavernier <matthieu.tavernier@epitech.eu>
2020-12-14 17:38:43 +0100

    ajax request added

--- experquiztest/bigquerycall/views.py
+++ experquiztest/bigquerycall/views.py
@@ -1,28 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from google.cloud import bigquery
-import re
+import json
 
 
 
 def index(request):
-    client = bigquery.Client()
-    query = (
-    'SELECT licenses.license as license,'
-    'count(*) as total '
-    'FROM `bigquery-public-data.github_repos.sample_repos` as repo inner join `bigquery-public-data.github_repos.licenses` as licenses on repo.repo_name = licenses.repo_name '
-    'GROUP BY license '
-    'order by total desc '
-    'LIMIT 5'
-    )
-    results = client.query(query)
-    licenses = []
-    totals = []
-    for row in results:
-        licenses.append(row.license)
-        totals.append(row.total)
-    context = {
-        'licenses': licenses,
-        'totals': totals
-    }
-    return render(request, 'bigquerycall/index.html', context)
+    return render(request, 'bigquerycall/index.html')
+
+def licenses(request):
+    if request.is_ajax():
+        client = bigquery.Client()
+        query = (
+        'SELECT licenses.license as license,'
+        'count(*) as total '
+        'FROM `bigquery-public-data.github_repos.sample_repos` as repo inner join `bigquery-public-data.github_repos.licenses` as licenses on repo.repo_name = licenses.repo_name '
+        'GROUP BY license '
+        'order by total desc '
+        'LIMIT 5'
+        )
+        results = client.query(query)
+        licenses = []
+        totals = []
+        for row in results:
+            licenses.append(row.license)
+            totals.append(row.total)
+        context = {
+            'licenses': licenses,
+            'totals': totals
+        }
+        return HttpResponse(json.dumps(context),
+            content_type='application/json')
+
+    return render(request, 'index.html', {'user': ''})
